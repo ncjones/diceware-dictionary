112d3db36c8410168706869ba4f9d37adb8615f0
Nathan Jones <nathan@ncjones.com>
2015-06-28 17:38:57 +1200

    output list of single diceware words

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/ncjones/diceware-dictionary"
   :license {:name "GNU General Public License"
             :url "https://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies
+      [[org.clojure/clojure "1.5.1"]
+       [clj-http "1.1.2"]]
   :main ^:skip-aot diceware-dictionary.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/diceware_dictionary/core.clj
+++ src/diceware_dictionary/core.clj
@@ -1,7 +1,35 @@
 (ns diceware-dictionary.core
-  (:gen-class))
+  (:require
+    clj-http.client
+    [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn get-body [url]
+  (:body (clj-http.client/get url)))
+
+(defn get-and-cache [url filename]
+  (if (.exists (clojure.java.io/file filename))
+    (slurp filename)
+    (let [body (get-body url)]
+      (spit filename body)
+      body)))
+
+(def dice-text
+  (get-and-cache
+    "http://world.std.com/~reinhold/diceware.wordlist.asc"
+    "diceware.wordlist.asc"))
+
+(def dice-text-lines
+  (clojure.string/split-lines dice-text))
+
+(def dice-lines
+  (filter #(re-matches #"^\d{5}.*" %) dice-text-lines))
+
+(defn second-tsv [s]
+  (nth (str/split s #"\t") 1))
+
+(def dice-words
+  (map second-tsv dice-lines))
+
+(defn -main [& args]
+  (doseq [line dice-words]
+    (println line)))
